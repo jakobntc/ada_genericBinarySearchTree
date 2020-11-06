6d6ad908f02a7eb69493f2d0325aafdbfdee65d8
jakobntc <jclapsaddle@radford.edu>
2020-11-05 22:38:23 -0500

    Added placeholders for the functions / procedures that have not been implemented yet.

--- bstpackage.adb
+++ bstpackage.adb
@@ -1,6 +1,9 @@
-package body is
+with Ada.Text_IO; use Ada.Text_IO;
+
+package body bstpackage is
 
     function isEmpty(Tree : BST) return Boolean is
+    begin
         if Tree.right = NULL then
             if Tree.Left = NULL then
                 return True;
@@ -12,8 +15,6 @@ package body is
         end if;
     end isEmpty;
 
-
-
     -- check to see if the tree is empty.
     -- if it is empty then the value would be made the root node of the BST.
     -- if it is not empty then the value would be placed in the correct location.
@@ -22,31 +23,60 @@ package body is
         begin
             if Item < n.Data then
                 if n.Left = NULL then
-                    return n;
+                    return n.all;
                 else
-                    addHelper(Item, n.Left);
+                    return addHelper(Item, n.Left);
                 end if;
             elsif Item > n.Data then
                 if n.Right = NULL then
-                    return n;
+                    return n.all;
                 else
-                    addHelper(Item, n.Right);
+                    return addHelper(Item, n.Right);
                 end if;
             end if;
         end;
-        newNode : Node := new Node;
+        newNode : NodePtr := new Node;
+        parentNode : Node;
     begin
         newNode.Data := Item;
         newNode.Left := NULL;
         newNode.Right := NULL;
 
-        if Item < Tree.Data then
-            parentNode : Node := addHelper(Item : ItemType; Tree.Left);
-            parentNode.Left := newNode;
-        elsif Item > Tree.Data then
-            parentNode : Node := addHelper(Item : ItemType; Tree.Right);
-            parentNode.Right := newNode;
+        if isEmpty(Tree) then
+            if Item < Tree.Data then
+                Tree.Left := newNode;
+            elsif Item > Tree.Data then
+                Tree.Right := newNode;
+            else
+                put_line("Item is already in the binary search tree.");
+            end if;
         else
-            put_line("Item is already in the binary search tree.");
+            if Item < Tree.Data then
+                parentNode := addHelper(Item, Tree.Left);
+                parentNode.Left := newNode;
+            elsif Item > Tree.Data then
+                parentNode := addHelper(Item, Tree.Right);
+                parentNode.Right := newNode;
+            else
+                put_line("Item is already in the binary search tree.");
+            end if;
         end if;
     end add;
+
+    procedure remove(Item : ItemType; Tree : in out BST) is
+    begin
+        NULL;
+    end remove;
+
+    function contains(Item : ItemType; Tree : BST) return Boolean is
+    begin
+        return True;
+    end contains;
+
+
+    procedure printInSortedOrder(Tree : BST) is
+    begin
+        NULL;
+    end printInSortedOrder;
+
+end bstpackage;
