f01027803a0c8ff5eb66108c657d3dd7a5169c9b
jakobntc <jclapsaddle@radford.edu>
2020-11-07 15:04:36 -0500

    remove is not working properly

--- bstpackage.adb
+++ bstpackage.adb
@@ -23,7 +23,6 @@ package body bstpackage is
         newNode : NodePtr := new Node;
         procedure addHelper(Item : ItemType; n : NodePtr) is
         begin
-
             if compare(Item, n.Data) < 0 then
                 if n.Left = NULL then
                     n.Left := newNode;
@@ -53,13 +52,104 @@ package body bstpackage is
     end add;
 
     procedure remove(Item : ItemType; Tree : in out BST) is
+        nodeToDelete : NodePtr;
+        previousNode : NodePtr;
+        replacmentData : ItemType;
+        valueFound : Boolean := False;
+        wentRight : Boolean := False;
+        wentLeft : Boolean := False;
+        replacementDone : Boolean := False;
+        procedure removeHelper(Item : ItemType; n : NodePtr) is
+        begin
+            if Item = n.Data then
+                put_line("Found the node to delete");
+                nodeToDelete := n;
+                valueFound := True;
+                put_line("checking if the node pointers are null or not.");
+                if n.Left /= NULL then
+                    put_line("Found the node to delete / left pointer wasn't null");
+                    wentLeft := True;
+                    previousNode := n;
+                    removeHelper(Item, n.Left);
+                elsif n.Right /= NULL then
+                    put_line("Found the node to delete / right pointer wasn't null");
+                    wentRight := True;
+                    previousNode := n;
+                    removeHelper(Item, n.Right);
+                elsif n.left = null and n.Right = null then
+                    put_line("Somehow you messed up big time.");
+                end if;
+            end if;
+            if wentLeft and not replacementDone then
+                put_line("wentLeft was true");
+                if n.Right /= NULL then
+                    put_line("right node wasn't null, going right.");
+                    previousNode := n;
+                    removeHelper(Item, n.Right);
+                else
+                    put_line("right node pointer was null so replacing data and whatnot.");
+                    replacmentData := n.Data;
+                    nodeToDelete.Data := replacmentData;
+                    previousNode.Right := NULL;
+                    replacementDone := True;
+                end if;
+            elsif wentRight and not replacementDone then
+                if n.Left /= NULL then
+                    put_line("left node wasn't null, going left.");
+                    previousNode := n;
+                    removeHelper(Item, n.Left);
+                else
+                    put_line("Left node pointer was null so replacing data and whatnot.");
+                    replacmentData := n.Data;
+                    nodeToDelete.Data := replacmentData;
+                    previousNode.Left := NULL;
+                    replacementDone := True;
+                end if;
+            else
+                put_line("Searching for data that needs to be deleted.");
+                if compare(Item, n.Data) < 0 then
+                    previousNode := n;
+                    removeHelper(Item, n.Left);
+                elsif compare(Item, n.Data) > 0 then
+                    previousNode := n;
+                    removeHelper(Item, n.Right);
+                end if;
+            end if;
+            
+        end;
     begin
-        NULL;
+        if isEmpty(Tree) then
+            put_line("Tree is empty");
+        else
+            removeHelper(Item, NodePtr(Tree));
+        end if;
     end remove;
 
     function contains(Item : ItemType; Tree : BST) return Boolean is
+        function containsHelper(Item : ItemType; n : NodePtr) return Boolean is
+            found : Boolean := False;
+        begin
+            if n.Data = Item then
+                found := True;
+            else
+                if compare(Item, n.Data) < 0 then
+                    put_line("Went left.");
+                    found := containsHelper(Item, n.Left);
+                end if;
+                if compare(Item, n.Data) > 0 then
+                    put_line("Went right.");
+                    found := containsHelper(Item, n.Right);
+                end if;
+            end if;
+            return found;
+        end;
     begin
-        return True;
+        if isEmpty(Tree) then
+            put_line("BST is empty idiot.");
+            return False;
+        else
+            return containsHelper(Item, NodePtr(Tree));
+        end if;
     end contains;
 
 
@@ -69,13 +159,27 @@ package body bstpackage is
     procedure printInSortedOrder(Tree : BST) is
         procedure printInSortedOrderHelper(n : NodePtr) is
         begin
-            null;
+            if n.right = NULL and n.Left = NULL then
+                printItem(n.Data);
+            elsif n.Right = NULL and n.Left /= NULL then
+                printInSortedOrderHelper(n.Left);
+                printItem(n.Data);
+            elsif n.Right /= NULL and n.Left = NULL then
+                printItem(n.Data);
+                printInSortedOrderHelper(n.Right);
+            else
+                printInSortedOrderHelper(n.Left);
+                printItem(n.Data);
+                printInSortedOrderHelper(n.Right);
+            end if;
         end;
     begin
-        
         if isEmpty(Tree) then
-            null;
-
+            put_line("Tree is empty idiot");
+        elsif Tree.Left = NULL and Tree.Right = NULL then
+            printItem(Tree.Data);
+        else
+            printInSortedOrderHelper(NodePtr(Tree));
         end if;
     end printInSortedOrder;
 
